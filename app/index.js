a556e971142adc94ffe1a5b92d4e9a46c24f7f97
Anshul Kharbanda <akanshul97@gmail.com>
2020-09-10 19:23:53 -0400

    Eradicated elon

--- app/index.js
+++ app/index.js
@@ -4,18 +4,3 @@
  * Author:  Anshul Kharbanda
  * Created: 9 - 10 - 2020
  */
-import './style/main.scss'
-import elon from './assets/images/elon.jpg'
-
-// Get app
-let app = document.getElementById('app')
-app.innerHTML = '<h1 id="title">Eyyyyy</h1>'
-    + `<img id="elon" src="${elon}">`
-    + '<button id="destupid">Make it less Stupid</button>'
-
-// Destupid button
-document.getElementById('destupid').addEventListener('click', function() {
-    document.getElementById('title').innerText = 'It\'s Elon!'
-    document.getElementById('elon').style.display = 'block'
-    document.getElementById('destupid').style.display = 'none'
-})

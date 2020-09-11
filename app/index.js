ff103e0a87410151a826b2cf4cd3d8f494ece0e4
Anshul Kharbanda <akanshul97@gmail.com>
2020-09-11 19:35:23 -0400

    Change animation after 10 seconds using observable systems

--- app/index.js
+++ app/index.js
@@ -5,24 +5,43 @@
  * Created: 9 - 10 - 2020
  */
 import './style/main.scss'
+import { animationFrameScheduler, of } from 'rxjs';
+import { observeOn, repeat } from 'rxjs/operators';
+import { buildRandomFourier } from './fourier/builders/random';
 import { buildSquareFourier } from './fourier/builders/square';
 import { FourierOutputContext } from './fourier-output';
+
+// Create an animation looper
+const animationLoop$ = of(0).pipe(
+    observeOn(animationFrameScheduler),
+    repeat()
+)
 
 // Draw Context
 let fctx = new FourierOutputContext('fourout')
 
-// Fourier initial
-let fourier = buildSquareFourier(4, 1.0)
-let path = fourier.getPath()
-let state = fourier.getInitialState()
-
-// Draw frame on every animation frame
-function drawFrame() {
-    fctx.clear()
-    fctx.drawPath(path)
-    fctx.drawLines(state)
-    fctx.drawCircles(state)
-    state = state.update()
-    requestAnimationFrame(drawFrame)
+function createAnimation(fourier, fctx) {
+    const path = fourier.getPath()
+    let state = fourier.getInitialState()
+    return () => {
+        fctx.clear()
+        fctx.drawPath(path)
+        fctx.drawLines(state)
+        fctx.drawCircles(state)
+        state = state.update()    
+    }
 }
-requestAnimationFrame(drawFrame)
+
+// Initial fourier animation
+let fourier = buildSquareFourier(4, 1.0)
+let fanim = createAnimation(fourier, fctx)
+
+// Frame animation
+let animation = animationLoop$.subscribe(fanim)
+
+setTimeout(() => {
+    animation.unsubscribe()
+    fourier = buildRandomFourier(4, 1.0)
+    fanim = createAnimation(fourier, fctx)
+    animation = animationLoop$.subscribe(fanim)
+}, 10000)

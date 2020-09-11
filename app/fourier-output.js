25386e4ec16140178aac48d8be8b870f43343f0a
Anshul Kharbanda <akanshul97@gmail.com>
2020-09-11 18:32:25 -0400

    Created Fourier Output Context object to handle drawing fourier things

---
+++ app/fourier-output.js
@@ -0,0 +1,85 @@
+/**
+ * A visual that demos drawing paths using a fourier series.
+ * 
+ * Author:  Anshul Kharbanda
+ * Created: 9 - 10 - 2020
+ */
+import { Vector } from './vector/vector'
+
+export class FourierOutputContext {
+    constructor(id, scale=50) {
+        this.canvas = document.getElementById(id)
+        this.ctx = this.canvas.getContext('2d')
+        this.width = this.canvas.width
+        this.height = this.canvas.height
+        this.scale = scale
+    }
+
+    getOrigin() {
+        return new Vector(this.width/2, this.height/2)
+    }
+
+    sCorrect(r) {
+        return r.times(this.scale)
+    }
+
+    correct(r) {
+        let b = r
+        b = this.sCorrect(b)
+        b = b.plus(this.getOrigin())
+        b = b.round()
+        return b
+    }
+
+    clear() {
+        this.ctx.clearRect(0, 0, this.width, this.height)
+    }
+
+    drawPath(path) {
+        this.ctx.lineWidth = '3'
+        this.ctx.strokeStyle = '#045'
+        this.ctx.beginPath()
+        let r = this.correct(path[0])
+        this.ctx.moveTo(r.x, r.y)
+        for (let i = 1; i < path.length; i++) {
+            r = this.correct(path[i])
+            this.ctx.lineTo(r.x, r.y)
+        }
+        this.ctx.stroke()
+    }
+
+    drawLines(fourierState) {
+        // Initialize context
+        this.ctx.lineWidth = '1'
+        this.ctx.strokeStyle = '#fa0'
+
+        // Initialize path
+        let s = this.getOrigin()
+        this.ctx.beginPath()
+        this.ctx.moveTo(s.x, s.y)
+    
+        // Draw lines along path
+        fourierState.elements.forEach(({ vector }) => {
+            s = s.plus(this.sCorrect(vector))
+            this.ctx.lineTo(s.x, s.y)
+        })
+    
+        // Stroke
+        this.ctx.stroke()
+    }
+    
+    drawCircles(fourierState) {
+        // Initialize context
+        this.ctx.lineWidth = '0.5'
+        this.ctx.strokeStyle = '#555'
+        
+        // Draw circles along path
+        let s = this.getOrigin()
+        fourierState.elements.forEach(element => {
+            this.ctx.beginPath()
+            this.ctx.arc(s.x, s.y, element.scale*this.scale, 0, 2*Math.PI)
+            this.ctx.stroke()
+            s = s.plus(this.sCorrect(element.vector))
+        })
+    }    
+}
--- app/index.js
+++ app/index.js
@@ -5,70 +5,11 @@
  * Created: 9 - 10 - 2020
  */
 import './style/main.scss'
-import { Vector } from './vector/vector';
 import { FourierSeries } from './fourier/series';
+import { FourierOutputContext } from './fourier-output';
 
-// Get draw context
-const fout = document.getElementById('fourout')
-const fctx = fout.getContext('2d')
-const width = fout.width
-const height = fout.height
-const scale = 50
-
-// Correction things
-const ORIGIN = new Vector(width/2, height/2)
-const sCorrect = a => a.times(scale)
-const aCorrect = a => a.plus(ORIGIN)
-const rCorrect = a => a.round()
-const correct = a => rCorrect(aCorrect(sCorrect(a)))
-
-function drawPath(path) {
-    fctx.lineWidth = '3'
-    fctx.strokeStyle = '#045'
-    fctx.beginPath()
-    let r = correct(path[0])
-    fctx.moveTo(r.x, r.y)
-    for (let i = 1; i < path.length; i++) {
-        r = correct(path[i])
-        fctx.lineTo(r.x, r.y)
-    }
-    fctx.stroke()
-}
-
-function drawLines(fourierState) {
-    // Initialize context and path
-    let s = ORIGIN
-    fctx.lineWidth = '1'
-    fctx.strokeStyle = '#fa0'
-    fctx.beginPath()
-    fctx.moveTo(s.x, s.y)
-
-    // Draw lines along path
-    fourierState.elements.forEach(({ vector }) => {
-        s = s.plus(sCorrect(vector))
-        fctx.lineTo(s.x, s.y)
-    })
-
-    // Stroke
-    fctx.stroke()
-}
-
-function drawCircles(fourierState) {
-    // Initialize context and path
-    let s = ORIGIN
-    fctx.lineWidth = '0.5'
-    fctx.strokeStyle = '#555'
-
-    // Draw circles along path
-    fourierState.elements.forEach(element => {
-        fctx.beginPath()
-        fctx.arc(s.x, s.y, element.scale*scale, 0, 2*Math.PI)
-        fctx.stroke()
-        s = s.plus(sCorrect(element.vector))
-    })
-}
-
-// ------------------------------- MAIN -------------------------------
+// Draw Context
+let fctx = new FourierOutputContext('fourout')
 
 // Fourier initial
 let fourier = FourierSeries.random(6, 1.0)
@@ -77,10 +18,10 @@ let state = fourier.getInitialState()
 
 // Draw frame on every animation frame
 function drawFrame() {
-    fctx.clearRect(0, 0, width, height)
-    drawPath(path)
-    drawLines(state)
-    drawCircles(state)
+    fctx.clear()
+    fctx.drawPath(path)
+    fctx.drawLines(state)
+    fctx.drawCircles(state)
     state = state.update()
     requestAnimationFrame(drawFrame)
 }
